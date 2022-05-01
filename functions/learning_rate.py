7671e9a7ccef26c5acd479c34e826a8749f1b9d5
Marlon Menendez <marlon.menendezg@gmail.com>
2022-05-01 17:17:46 -0600

    Add the notebook for resizing images in chapter 6

--- functions/learning_rate.py
+++ functions/learning_rate.py
@@ -1,10 +1,10 @@
 def exponential_decay(lr0, s):
     """Implements exponential decay of learning rate
-    
+
     n(t) = lr0 * 0.1 ** (epoch / s)
-    
+
     The function returns a function that will be passed to the callback.
-    
+
     Example:
 
         exponential_decay_fn = exponential_decay(lr0=0.1, s=20)
@@ -14,19 +14,21 @@ def exponential_decay(lr0, s):
         lr0 (float): Initial value of learning rate.
         s (int): Number of steps in which the learning rate will decay.
     """
-    
+
     def exponential_decay_fn(epoch):
         return lr0 * 0.1 ** (epoch / s)
-    
+
     return exponential_decay_fn
 
 
-def exponential_decay_with_warmup(lr_start=0.0001,
-                                  lr_max=0.0001, 
-                                  lr_min=0.00001,
-                                  lr_rampup_epochs=4, 
-                                  lr_sustain_epochs=1,
-                                  lr_exp_decay=0.8):
+def exponential_decay_with_warmup(
+    lr_start=0.0001,
+    lr_max=0.0001,
+    lr_min=0.00001,
+    lr_rampup_epochs=4,
+    lr_sustain_epochs=1,
+    lr_exp_decay=0.8,
+):
     """Implements exponential decay learning rate with warm up.
 
     Args:
@@ -37,16 +39,19 @@ def exponential_decay_with_warmup(lr_start=0.0001,
         lr_sustain_epochs (int, optional): Number of epochs the learning rate will be equal to lr_max. Defaults to 1.
         lr_exp_decay (float, optional): Factor in which the learning rate will decay. Defaults to 0.8.
     """
+
     def exponential_decay_fn(epoch):
         if epoch < lr_rampup_epochs:
             lr = (lr_max - lr_start) / lr_rampup_epochs * epoch + lr_start
         elif epoch < (lr_rampup_epochs + lr_sustain_epochs):
             lr = lr_max
         else:
-            lr = (lr_max - lr_min) * lr_exp_decay ** (epoch - lr_rampup_epochs - lr_sustain_epochs) + lr_min
-        
+            lr = (lr_max - lr_min) * lr_exp_decay ** (
+                epoch - lr_rampup_epochs - lr_sustain_epochs
+            ) + lr_min
+
         return lr
-    
+
     return exponential_decay_fn
 
 
@@ -56,7 +61,7 @@ def piecewise_constant_fn(epoch):
     Args:
         epoch (int): Number of the current epoch.
     """
-    
+
     if epoch < 5:
         return 0.01
     elif epoch < 15:
